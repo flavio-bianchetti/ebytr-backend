9bfa471817649eb521928e7373cd8430797b6fbf
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-18 17:00:16 -0300

    feat: User model setup included

---
+++ src/models/user.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from 'sequelize';
+import database from '.';
+
+class User extends Model {
+  public id!: number;
+
+  public name!: string;
+
+  public email!: string;
+
+  public password!: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.NUMBER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  }
+}, {
+  underscored: true,
+  sequelize: database,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
