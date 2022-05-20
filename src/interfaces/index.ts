36b3332154b308487aa4666d6ef017b8442bde9e
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-19 23:11:12 -0300

    feat: Interface index export updated

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,15 @@
+import ITodo from './ITodo';
+import ITodoCreate from './ITodoCreate';
+import ITodoUpdate from './ITodoUpdate';
 import IUser from './IUser';
+import IUserCreate from './IUserCreate';
 import IUserToken from './IUserToken';
 
 export {
+  ITodo,
+  ITodoCreate,
+  ITodoUpdate,
   IUser,
+  IUserCreate,
   IUserToken,
 };
