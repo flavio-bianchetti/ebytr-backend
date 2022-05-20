04ad066e885a391b89e664b6622091fb0220e743
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-19 23:12:09 -0300

    fix: UserController validation removed

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from 'express';
-import { IUser } from '../interfaces';
+import { IUser, IUserCreate } from '../interfaces';
 import { UserService } from '../services';
 
 export default class UserController {
   public static create = async (req: Request, res: Response): Promise<Response> => {
     try {
-      const { name, email, password }: IUser = req.body;
-      if (!password) return res.status(402).json({ message: 'Password is required' });
+      const { name, email, password }: IUserCreate = req.body;
       const result = await UserService.create(name, email, password);
       if (result === undefined) return res.status(409).json({ message: 'E-mail already exists' });
       if (result === null) return res.status(422).json({ message: 'Encryption failure' });
