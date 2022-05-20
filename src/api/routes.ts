34d74abbdc5d82460d0b722415cac8ecb34e0e88
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-19 23:15:15 -0300

    feat: Routes updated

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,13 +1,43 @@
 import { Router } from 'express';
 import * as cors from 'cors';
-import { UserController } from '../controllers';
+import { UserController, TodoController } from '../controllers';
+import { ValidateJWTMiddleware, ValidateRoutesEntries } from '../middlewares';
 
 const routes = Router();
 
 routes.use(cors());
 
 routes.post('/login', UserController.login);
+routes.post(
+  '/create',
+  ValidateRoutesEntries.validateCreateUser,
+  UserController.create
+);
 
-routes.post('/create', UserController.create);
+routes.post(
+  '/todolist',
+  ValidateRoutesEntries.validateCreateTodoTask,
+  ValidateJWTMiddleware.validate,
+  TodoController.create,
+);
+routes.get(
+  '/todolist/:id',
+  ValidateRoutesEntries.validateParamId,
+  ValidateJWTMiddleware.validate,
+  TodoController.findAll,
+);
+routes.put(
+  '/todolist/task/:id',
+  ValidateRoutesEntries.validateParamId,
+  ValidateRoutesEntries.validateUpdateTodoTask,
+  ValidateJWTMiddleware.validate,
+  TodoController.update,
+);
+routes.delete(
+  '/todolist/task/:id',
+  ValidateRoutesEntries.validateParamId,
+  ValidateJWTMiddleware.validate,
+  TodoController.delete,
+);
 
 export default routes;
